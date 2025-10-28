d62dcf1a54fc29f0b8e842ba9243cf0c701ec175
Yudann <yudannnnn80@gmail.com>
2025-10-28 20:31:57 +0700

    feat: jus first commit yeah

---
+++ src/app/map/page.tsx
@@ -0,0 +1,7 @@
+export default function MapPage() {
+  return (
+    <div>
+      <h1>ini adalah halmaan map</h1>
+    </div>
+  );
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,7 @@
+export default function RegitserPage() {
+  return (
+    <div>
+      <h1>ini adalah halmaan regis</h1>
+    </div>
+  );
+}
