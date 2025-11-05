3013f5d01a7fc26bb4f61c6dbefe7feea2e493de
Yudann <yudannnnn80@gmail.com>
2025-11-05 14:55:31 +0700

    konfigurasi: setup supabase buat backend

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,13 @@
+import { Database } from '@/integration/supabase/types';
+import { createClient } from '@supabase/supabase-js';
+
+// Next.js environment variables
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const SUPABASE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
+
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+});
